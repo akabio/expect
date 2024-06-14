a15a7add646c007bcb3e28dcd4e160675082aa9b
Christian Reichlin <chrigi@kerbtier.ch>
2024-06-14 14:48:11 +0200

    image snapshot poc

--- snapshot.go
+++ snapshot.go
@@ -1,8 +1,14 @@
 package expect
 
 import (
+	"bytes"
+	"image"
+	"image/png"
 	"os"
 	"path/filepath"
+
+	_ "image/gif"
+	_ "image/jpeg"
 
 	"golang.org/x/exp/slices"
 )
@@ -53,4 +59,89 @@ func (e Val) ToBeSnapshot(path string) Val {
 	}
 
 	return e
+}
+
+func (e Val) ToBeSnapshotImage(path string) Val {
+	e.t.Helper()
+
+	folder := filepath.Dir(path)
+	if folder != "." {
+		err := os.MkdirAll(folder, 0o755)
+		if err != nil {
+			e.t.Fatalf("failed to create target folder %v", folder)
+		}
+	}
+
+	existing, err := os.ReadFile(path)
+	if err != nil && !os.IsNotExist(err) {
+		e.t.Fatalf("failed to read snaphsot %v: %v", path, err)
+	}
+
+	img, isImage := e.value.(image.Image)
+	if !isImage {
+		e.t.Fatalf("value of .ToBeSnaphsotImage must be of type image but it is %T", e.value)
+	}
+
+	// snapshot does not exist, create it
+	if existing == nil {
+		encoded := bytes.NewBuffer(nil)
+
+		err = png.Encode(encoded, img)
+		if err != nil {
+			e.t.Fatalf("failed encode snapshot image %v", path)
+		}
+
+		err = os.WriteFile(path, encoded.Bytes(), 0o644)
+		if err != nil {
+			e.t.Fatalf("failed to write snapshot %v", path)
+		}
+
+		return e
+	}
+
+	// snapshot exists, compare it
+	encoded := bytes.NewBuffer(existing)
+	snapshotImage, _, err := image.Decode(encoded)
+	if err != nil {
+		e.t.Fatalf("failed to read snapshot %v", err)
+	}
+
+	if isSameImage(e.t, snapshotImage, img) {
+		// all is well, snapshot is matched, remove a possible current version
+		os.RemoveAll(path + ".current")
+		return e
+	}
+
+	// not the same image, write current output
+	err = png.Encode(encoded, img)
+	if err != nil {
+		e.t.Fatalf("failed encode snapshot image %v", path+".current")
+	}
+
+	err = os.WriteFile(path+".current", encoded.Bytes(), 0o644)
+	if err != nil {
+		e.t.Fatalf("failed to write snapshot %v", path+".current")
+	}
+
+	return e
+}
+
+func isSameImage(t Test, snapshot, current image.Image) bool {
+	snapshotSize := snapshot.Bounds().Size()
+	currentSize := current.Bounds().Size()
+	if snapshotSize != currentSize {
+		t.Errorf("expected image size to be %v but it is %v", snapshotSize, currentSize)
+		return false
+	}
+
+	for y := 0; y < snapshotSize.Y; y++ {
+		for x := 0; x < snapshotSize.X; x++ {
+			if snapshot.At(x, y) != current.At(x, y) {
+				t.Errorf("images are not the same")
+				return false
+			}
+		}
+	}
+
+	return true
 }
--- snapshot_test.go
+++ snapshot_test.go
@@ -1,6 +1,8 @@
 package expect_test
 
 import (
+	"bytes"
+	"image"
 	"os"
 	"testing"
 
@@ -8,60 +10,99 @@ import (
 	"github.com/akabio/expect/internal/test"
 )
 
+var sampleImage = func() image.Image {
+	sp, err := os.ReadFile("./testdata/sample.png")
+	if err != nil {
+		panic(err)
+	}
+
+	img, _, err := image.Decode(bytes.NewBuffer(sp))
+	if err != nil {
+		panic(err)
+	}
+
+	return img
+}()
+
 func TestCreateSnapshot(t *testing.T) {
 	cleanTestData(t)
-	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/ss1.txt")
+	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/volatile/ss1.txt")
 
-	data, err := os.ReadFile("testdata/ss1.txt")
+	data, err := os.ReadFile("testdata/volatile/ss1.txt")
 	expect.Error(t, err).ToBe(nil)
 	expect.Value(t, "content", string(data)).ToBe("we are all crazy")
 }
 
 func TestMismatchSnapshot(t *testing.T) {
 	cleanTestData(t)
-	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/ss1.txt")
+	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/volatile/ss1.txt")
 	l := test.New(t, func(t expect.Test) {
-		expect.Value(t, "content", "we are all nuts").ToBeSnapshot("testdata/ss1.txt")
+		expect.Value(t, "content", "we are all nuts").ToBeSnapshot("testdata/volatile/ss1.txt")
 	})
-	l.ExpectMessage(0).ToBe("snapshot for testdata/ss1.txt does not match current output")
+	l.ExpectMessage(0).ToBe("snapshot for testdata/volatile/ss1.txt does not match current output")
 
-	data, err := os.ReadFile("testdata/ss1.txt")
+	data, err := os.ReadFile("testdata/volatile/ss1.txt")
 	expect.Error(t, err).ToBe(nil)
 	expect.Value(t, "content", string(data)).ToBe("we are all crazy")
 
-	data, err = os.ReadFile("testdata/ss1.txt.current")
+	data, err = os.ReadFile("testdata/volatile/ss1.txt.current")
 	expect.Error(t, err).ToBe(nil)
 	expect.Value(t, "content", string(data)).ToBe("we are all nuts")
 }
 
 func TestMatchAfterMismatchSnapshot(t *testing.T) {
 	cleanTestData(t)
-	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/ss1.txt")
+	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/volatile/ss1.txt")
 	test.New(t, func(t expect.Test) {
-		expect.Value(t, "content", "we are all nuts").ToBeSnapshot("testdata/ss1.txt")
+		expect.Value(t, "content", "we are all nuts").ToBeSnapshot("testdata/volatile/ss1.txt")
 	})
-	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/ss1.txt")
+	expect.Value(t, "content", "we are all crazy").ToBeSnapshot("testdata/volatile/ss1.txt")
 
-	data, err := os.ReadFile("testdata/ss1.txt")
+	data, err := os.ReadFile("testdata/volatile/ss1.txt")
 	expect.Error(t, err).ToBe(nil)
 	expect.Value(t, "content", string(data)).ToBe("we are all crazy")
 
-	data, err = os.ReadFile("testdata/ss1.txt.current")
-	expect.Error(t, err).Message().ToBe("open testdata/ss1.txt.current: no such file or directory")
+	data, err = os.ReadFile("testdata/volatile/ss1.txt.current")
+	expect.Error(t, err).Message().ToBe("open testdata/volatile/ss1.txt.current: no such file or directory")
 }
 
 func TestCreateSnapshotFromBytes(t *testing.T) {
 	cleanTestData(t)
-	expect.Value(t, "content", []byte{1, 2, 3}).ToBeSnapshot("testdata/ss1.bin")
+	expect.Value(t, "content", []byte{1, 2, 3}).ToBeSnapshot("testdata/volatile/ss1.bin")
 
-	data, err := os.ReadFile("testdata/ss1.bin")
+	data, err := os.ReadFile("testdata/volatile/ss1.bin")
 	expect.Error(t, err).ToBe(nil)
 	expect.Value(t, "content", data).ToBe([]byte{1, 2, 3})
 }
 
 func cleanTestData(t *testing.T) {
-	err := os.RemoveAll("testdata")
+	err := os.RemoveAll("testdata/volatile")
 	if err != nil {
 		t.Fatal("Failed to clear testdata folder")
 	}
+}
+
+func TestCreateSnapshotImage(t *testing.T) {
+	cleanTestData(t)
+	expect.Value(t, "content", sampleImage).ToBeSnapshotImage("testdata/sample.png")
+}
+
+func TestCreateSnapshotImageSize(t *testing.T) {
+	cleanTestData(t)
+
+	l := test.New(t, func(t expect.Test) {
+		expect.Value(t, "content", sampleImage).ToBeSnapshotImage("testdata/snapshots/sample-small.png")
+	})
+	l.ExpectMessages().ToCount(1)
+	l.ExpectMessage(0).ToBe("expected image size to be (64,64) but it is (128,128)")
+}
+
+func TestCreateSnapshotImageDifferent(t *testing.T) {
+	cleanTestData(t)
+
+	l := test.New(t, func(t expect.Test) {
+		expect.Value(t, "content", sampleImage).ToBeSnapshotImage("testdata/snapshots/sample-dirty.png")
+	})
+	l.ExpectMessages().ToCount(1)
+	l.ExpectMessage(0).ToBe("images are not the same")
 }
